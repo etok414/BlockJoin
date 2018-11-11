058d3b0c2dfaa3ac4bb01f91a8f36ef0fd62e1a7
Mads Horn <madshorn@operamail.com>
2018-11-11 21:52:52 +0100

    Added dummy graphics and a proto type for drawing the board. Added a cube and asdw-movement.

--- testing_ground.py
+++ testing_ground.py
@@ -1,23 +1,58 @@
-import sys, pygame
+import sys
+import pygame
 
 pygame.init()
 
-columns, rows = 6, 6
 size = width, height = 800, 800
-black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption('BlockJoin')
 
-# ball = pygame.image.load("graphics\\floor_tile.png")
-img = pygame.image.load("graphics\\floor_tile.png")
-screen.fill(black)
-for x in range(columns):
-    for y in range(rows):
-        xi, yi = x - y, (x + y) / 2
-        tile = img.get_rect(center=(xi * 50 + 400, yi * 50 + 200))
-        screen.blit(img, tile)
 
-pygame.display.flip()
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+def main():
+    columns, rows = 6, 6
+    img1 = pygame.image.load("graphics\\floor_tile.png")
+    img2 = pygame.image.load("graphics\\cube.png")
+
+    xpos, ypos = 400, 200
+    tile2 = img2.get_rect(center=(xpos, ypos))
+    while 1:
+        draw_board(columns, rows, img1)
+        pygame.event.pump()
+        key = pygame.key.get_pressed()  # checking pressed keys
+        if key[pygame.K_a]:
+            print('rap')
+            xpos -= 50
+        if key[pygame.K_s]:
+            print('rup')
+            ypos += 50
+        if key[pygame.K_d]:
+            print('rip')
+            xpos += 50
+        if key[pygame.K_w]:
+            print('rop')
+            ypos -= 50
+        if key[pygame.K_q]:
+            sys.exit()
+        xpos1, ypos1 = xpos - ypos, (xpos + ypos) / 2
+        tile2.left, tile2.top = xpos1, ypos1
+        screen.blit(img2, tile2)
+
+        for event in pygame.event.get():  # Close nicely and display changes
+            if event.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+        pygame.time.delay(100)
+
+
+def draw_board(columns, rows, img1):
+    screen.fill((0, 0, 0))  # black
+    for x in range(columns):
+        for y in range(rows):
+            xi, yi = x - y, (x + y) / 2
+            tile1 = img1.get_rect(center=(xi * 50 + 400, yi * 50 + 200))
+            screen.blit(img1, tile1)
+
+
+if __name__ == '__main__':
+    main()
