90140f42ef1e2542cafb26880207df0ee812b2ad
HappyDustbunny <madshorn@operamail.com>
2018-11-12 22:14:26 +0100

    Made new tiles and a frog. Abandoned alfa-channel and used pygames colorkey instead.

--- testing_ground.py
+++ testing_ground.py
@@ -6,15 +6,27 @@ pygame.init()
 size = width, height = 800, 800
 
 screen = pygame.display.set_mode(size)
+screen.set_colorkey((0, 0, 0))
 pygame.display.set_caption('BlockJoin')
 
 
 def main():
     columns, rows = 6, 6
-    img1 = pygame.image.load("graphics\\floor_tile.png")
-    img2 = pygame.image.load("graphics\\cube.png")
+    img1 = pygame.image.load("graphics\\floor_tile1.png").convert()
+    img1.set_colorkey((0, 0, 0))
+    # img2 = pygame.image.load("graphics\\cube1.png").convert()
+    # img2.set_colorkey((0, 0, 0))
+    frog_e = pygame.image.load("graphics\\frogE.png").convert()
+    frog_e.set_colorkey((0, 0, 0))
+    frog_s = pygame.image.load("graphics\\frogS.png").convert()
+    frog_s.set_colorkey((0, 0, 0))
+    frog_w = pygame.image.load("graphics\\frogW.png").convert()
+    frog_w.set_colorkey((0, 0, 0))
+    frog_n = pygame.image.load("graphics\\frogN.png").convert()
+    frog_n.set_colorkey((0, 0, 0))
 
-    xpos, ypos = 400, 200
+    xpos, ypos = 420, 220
+    img2 = frog_e
     tile2 = img2.get_rect(center=(xpos, ypos))
     while 1:
         draw_board(columns, rows, img1)
@@ -22,12 +34,16 @@ def main():
         key = pygame.key.get_pressed()  # checking pressed keys
         if key[pygame.K_a]:
             xpos -= 50
+            img2 = frog_w
         if key[pygame.K_s]:
             ypos += 50
+            img2 = frog_s
         if key[pygame.K_d]:
             xpos += 50
+            img2 = frog_e
         if key[pygame.K_w]:
             ypos -= 50
+            img2 = frog_n
         if key[pygame.K_q]:
             sys.exit()
         xpos1, ypos1 = xpos - ypos, (xpos + ypos) / 2  # Transformation cartesian --> isometric
