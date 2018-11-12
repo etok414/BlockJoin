d390eaadaf039f85e0459a43a5702115b5f8fbbb
etok414 <etok414@gmail.com>
2018-11-12 10:54:18 +0100

    Combined the Player and Board classes.

--- blockjoin.py
+++ blockjoin.py
@@ -1,8 +1,8 @@
 import random
 
 
-class Player:
-    def __init__(self):
+class Game:
+    def __init__(self, height, width):
         self.carried_block = False
         self.x_pos = 0
         self.y_pos = 0
@@ -11,14 +11,19 @@ class Player:
         self.status = 'Fine'
         # Status is 'Fine' if the player is on the ground, 'Elevated' if it's on a block, and if it's holding a block,
         # that's its status.
+        self.bag = [0, 1, 2, 3]
+        random.shuffle(self.bag)
+        self.blocks = {}
+        self.board_height = height
+        self.board_width = width
 
-    def receive_input(self, recieved_input, board):
+    def receive_input(self, received_input):
         pass
 
-    def move(self, direction, board):
+    def move(self, direction):
         # TODO: Movement animation.
         moved_coors = (self.x_pos + self.dir_dict[direction][0], self.y_pos + self.dir_dict[direction][0])
-        probe_result = board.probe(moved_coors)
+        probe_result = self.probe(moved_coors)
 
         if probe_result and self.direction == direction:
             if probe_result == 'Occupied':
@@ -38,39 +43,29 @@ class Player:
             if self.status != 'Fine' and self.status != 'Elevated':
                 self.status.orientation = direction
 
-    def push(self, board):
+    def push(self):
         if self.status == 'Fine':
             # TODO: Punch animation.
-            relevant_dir = (self.dir_dict[self.direction][0], self.dir_dict[self.direction][0])
-            if board.probe((self.x_pos + relevant_dir[0], self.y_pos + relevant_dir[1])) == 'Occupied' \
-                    and board.probe((self.x_pos + relevant_dir[0]*2, self.y_pos + relevant_dir[1]*2)) == 'Empty':
-                board.blocks[(self.x_pos + relevant_dir[0]*2, self.y_pos + relevant_dir[1]*2)] = board.blocks[
-                    (self.x_pos + relevant_dir[0], self.y_pos + relevant_dir[1])]
-                board.blocks[(self.x_pos + relevant_dir[0], self.y_pos + relevant_dir[1])] = None
+            pf_space = (self.x_pos + self.dir_dict[self.direction][0], self.y_pos + self.dir_dict[self.direction][0])
+            pt_space = (self.x_pos + self.dir_dict[self.direction][0] * 2,
+                        self.y_pos + self.dir_dict[self.direction][0] *2)
+            if self.probe(pf_space) == 'Occupied' and self.probe(pt_space) == 'Empty':
+                self.blocks[pt_space] = self.blocks[pf_space]
+                self.blocks[pf_space] = None
         elif self.status == 'Elevated':
             pass
             # TODO: Punch animation.
         else:
-            board.blocks[(self.x_pos, self.y_pos)] = self.status
+            self.blocks[(self.x_pos, self.y_pos)] = self.status
             moved_coors = (self.x_pos - self.dir_dict[self.direction][0], self.y_pos - self.dir_dict[self.direction][0])
-            probe_result = board.probe(moved_coors)
+            probe_result = self.probe(moved_coors)
             if probe_result:
                 self.x_pos, self.y_pos = moved_coors
 
-            if board.probe((self.x_pos, self.y_pos)) == 'Occupied':
+            if self.probe((self.x_pos, self.y_pos)) == 'Occupied':
                 self.status = 'Elevated'
             else:
                 self.status = 'Fine'
-
-
-
-class Board:
-    def __init__(self, height, width):
-        self.bag = [0, 1, 2, 3]
-        random.shuffle(self.bag)
-        self.blocks = {}
-        self.height = height
-        self.width = width
 
     def make_block(self, orientation, is_ghost=False):
         pass
